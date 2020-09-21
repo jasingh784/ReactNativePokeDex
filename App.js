d4f06b179e3eb06293169138be19e56a87e74756
jasingh784 <48878742+jasingh784@users.noreply.github.com>
2020-09-20 17:52:20 -0700

    app fetches a pokemon from the api and displays its picture

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Fragment } from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {},
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://pokeapi.co/api/v2/pokemon/1')
+      .then(res => res.json())
+      .then((res) => {
+        this.setState({data: res});
+      })
+      .catch((error) => console.log(error))
+      .finally(() => {
+        this.setState({isLoading: false});
+        
+      });
+  }
+
+  render() {
+    const { data, isLoading } = this.state;
+
+    return (
+      
+      <View style={styles.container}>
+        {isLoading ? <ActivityIndicator/> : (
+        <Fragment>
+          <Text>PokeDex!</Text>
+          <Image source={{uri: data.sprites.front_default}}
+                  style={{width: 400, height: 400}}/>
+          <StatusBar style="auto" />
+          </Fragment>
+        )}
+        
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
